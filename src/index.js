9bee4d55bb2182008ed755b42d42cf2118512f82
Alex Kasongo <alex.kasongolenge@yahoo.com>
2021-01-02 21:24:35 -0800

    update comments

--- src/index.js
+++ src/index.js
@@ -10,32 +10,35 @@ const createCard = ({ title, href, img }) => `
     </div>
 `;
 
-fetch('movies.json')
-    // convert to json  
-    .then(res => res.json())
-    .then(movies => {
-        // sort movies
-        movies.sort((a, b) => a.title.localeCompare(b.title));
+fetch("movies.json")
+  // convert to json here
+  .then((res) => res.json())
+  .then((movies) => {
+    // sort movies
+    movies.sort((a, b) => a.title.localeCompare(b.title));
 
-        const cards = document.getElementById('cards')
-        const elRow = document.createElement('div');
-        elRow.className = 'row';
-        // add a child element to another element, in this case append the row to the card element
-        cards.appendChild(elRow);
+    const cards = document.getElementById("cards");
+    const elRow = document.createElement("div");
+    elRow.className = "row";
+    // add a child element to another element, in this case append the row to the card element
+    cards.appendChild(elRow);
 
-        const updateMovies = (movies) => {
-            elRow.innerHTML = movies.map((card) => `<div class="col-6 col-lg-3 mt-3">${card}</div>`).join('');
-        };
+    const updateMovies = (movies) => {
+      elRow.innerHTML = movies
+        .map((card) => `<div class="col-6 col-lg-3 mt-3">${card}</div>`)
+        .join("");
+    };
 
-        // gives us back cards html element
-        updateMovies(movies.map((movie) => createCard(movie)));
+    // gives us back cards html element
+    updateMovies(movies.map((movie) => createCard(movie)));
 
-        const search = document.querySelector('.search');
-        search.addEventListener('keyup', (evt) => {
-            const text = evt.target.value.toLowerCase();
-            updateMovies(movies
-                .filter(({ title }) => title.toLowerCase().includes(text))
-                .map(movie => createCard(movie))
-            );
-        });
+    const search = document.querySelector(".search");
+    search.addEventListener("keyup", (evt) => {
+      const text = evt.target.value.toLowerCase();
+      updateMovies(
+        movies
+          .filter(({ title }) => title.toLowerCase().includes(text))
+          .map((movie) => createCard(movie))
+      );
     });
+  });
